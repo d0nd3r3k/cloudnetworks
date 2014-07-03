0f664cefaf8948ccba4954ff5012fab0d01b5a90
Donald Derek <donaldderek.haddad@gmail.com>
2014-07-03 13:14:31 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -21,54 +21,26 @@ module.exports = {
       name: 'Cloudnetworks Cloud Core - Development'
     },
     facebook: {
-      clientID: "371211122956820",
-      clientSecret: "5c7850b638eae37894c55d42237782a9",
+      clientID: "",
+      clientSecret: "",
       callbackURL: "http://localhost:8080/auth/facebook/callback"
     },
     twitter: {
-      clientID: "IDxMydmv0kbWRgH0ESGpNQ",
-      clientSecret: "OCGTkWcUWsGs75Ca9vDoEvmrnXon7ELO3hbMsbg6OFU",
+      clientID: "",
+      clientSecret: "",
       callbackURL: "http://localhost:8080/auth/twitter/callback"
     },
     github: {
-      clientID: 'edff7f6fa8786db9c78a',
-      clientSecret: '1cafc8709a10cb33d98f6d34f0c94fc44c8116a1',
+      clientID: '',
+      clientSecret: '',
       callbackURL: 'http://localhost:8080/auth/github/callback'
     },
     google: {
-      clientID: "33125564461.apps.googleusercontent.com",
-      clientSecret: "L1qzCcoCzMWihIPacyjbHeOB",
+      clientID: "",
+      clientSecret: "",
       callbackURL: "http://localhost:8080/auth/google/callback"
     },
   },
   //TODO: fix API keys & Secret
-  production: {
-    db: 'mongodb://127.0.0.1/cloudnetworks',
-    root: rootPath,
-    notifier: notifier,
-    app: {
-      name: 'Cloudnetworks Core - Production'
-    },
-    facebook: {
-      clientID: "371211122956820",
-      clientSecret: "5c7850b638eae37894c55d42237782a9",
-      callbackURL: "http://50.116.27.203:8080/auth/facebook/callback"
-    },
-    twitter: {
-      clientID: "IDxMydmv0kbWRgH0ESGpNQ",
-      clientSecret: "OCGTkWcUWsGs75Ca9vDoEvmrnXon7ELO3hbMsbg6OFU",
-      callbackURL: "http://50.116.27.203:8080/auth/twitter/callback"
-    },
-    github: {
-      clientID: 'edff7f6fa8786db9c78a',
-      clientSecret: '1cafc8709a10cb33d98f6d34f0c94fc44c8116a1',
-      callbackURL: 'http://50.116.27.203:8080/auth/github/callback'
-    },
-    google: {
-      clientID: "33125564461.apps.googleusercontent.com",
-      clientSecret: "L1qzCcoCzMWihIPacyjbHeOB",
-      callbackURL: "http://50.116.27.203:8080/auth/google/callback"
-    },
-    
-  }
+  
 }
